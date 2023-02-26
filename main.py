a1f0f8147d9e6c935e757dd22d57cc74be68ffe8
AmrIbrahim22 <121577414+Amrrmiro@users.noreply.github.com>
2023-02-26 18:20:24 +0000

    McDonald's Chatbot assistant

--- main.py
+++ main.py
@@ -1,20 +1,69 @@
-def chatbot():
-  name = input("Chatbot: What is your name? ")
-  
-  if name == "Messi":
-    print('Nice to meet you GOAT')
-  else:
-    print(f"Chatbot: Nice to meet you, {name}! How are you today?")
-  while True:
-    user_input = input('You:')
-    if user_input == "Nice to meet you too! What's your name?":
-      print("Thanks! I am chatbot, You can talk to me about anything you want.")
-    elif user_input == "Who is the goat":
-      print('Messi without any doubt.')
-    elif user_input == "How to learn coding?":
-      print('Join Code2College.')
-    else:
-      print("Sorry! I can't help with this topic. Ask Google.")
-      break
+import time
 
-chatbot()
+# Menu & prices
+menu = {
+    "Big Mac": 3.99,
+    "Quarter Pounder": 4.99,
+    "McChicken": 3.49,
+    "French Fries": 1.49,
+    "McFlurry": 2.49,
+    "Coca Cola": 1.00,
+    "Sprite": 1.00,
+    "Fanta": 1.00,
+    "Water": 0.99
+}
+
+# Order calculator
+def calculate_total_price(order):
+    total_price = 0
+    for item in order:
+        total_price += menu[item]
+    return total_price
+
+# Delivery
+def simulate_delivery():
+    print("Your order is being prepared...")
+    time.sleep(3)
+    print("Your order is on the way!")
+    time.sleep(5)
+    print("Your order has been delivered. Enjoy your meal!")
+
+
+def process_input(user_input):
+    if "menu" in user_input:
+        print("Here's our menu:")
+        for item, price in menu.items():
+            print(f"{item}: ${price}")
+    elif "order" in user_input:
+        order = []
+        while True:
+            item = input("What would you like to order? ")
+            if item in menu:
+                order.append(item)
+                print(f"{item} has been added to your order.")
+            else:
+                print(f"Sorry, we don't have {item} in our menu.")
+            more_items = input("Would you like to order more? (yes/no) ")
+            if more_items.lower() == "no":
+                break
+        total_price = calculate_total_price(order)
+        print("Here's your order:")
+        for item in order:
+            print(item)
+        print(f"Total price: ${total_price}")
+        delivery_confirmation = input("Would you like us to deliver your order? (yes/no) ")
+        if delivery_confirmation.lower() == "yes":
+            simulate_delivery()
+    elif "track" in user_input or "delivery" in user_input:
+        print("Sorry, we don't have a tracking system at the moment.")
+    else:
+        print("Sorry, I didn't understand that.")
+
+# Start of the program
+print("Welcome to McDonald's! For menu type 'menu', To order type 'order'.")
+while True:
+    user_input = input("> ").lower()
+    if user_input == "exit":
+        break
+    else:
+        process_input(user_input)
